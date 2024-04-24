8d927a613b8d225130a0c330e05fbf17ed5e8303
FIOdawa <159128217+FIOdawa@users.noreply.github.com>
2024-04-24 11:22:28 +0200

    Added API

--- src/app.js
+++ src/app.js
@@ -1,10 +1,29 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function findCity(city) {
+  let apiKey = "fe210b17f1642aob305t6dda70162c42";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function actionSearchSubmit(event) {
   event.preventDefault();
 
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  findCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", actionSearchSubmit);
+
+findCity("London");
